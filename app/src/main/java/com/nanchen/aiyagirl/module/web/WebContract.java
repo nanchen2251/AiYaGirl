496a6891e18a04bf19794122fb9fd5144005f616
LiuShilin <503233512@qq.com>
2017-04-14 15:08:10 +0800

    加入webView

---
+++ app/src/main/java/com/nanchen/aiyagirl/module/web/WebContract.java
@@ -0,0 +1,10 @@
+package com.nanchen.aiyagirl.module.web;
+
+/**
+ * Author: nanchen
+ * Email: liushilin520@foxmail.com
+ * Date: 2017-04-14  14:38
+ */
+
+public class WebContract {
+}
---
+++ app/src/main/java/com/nanchen/aiyagirl/module/web/WebPresenter.java
@@ -0,0 +1,10 @@
+package com.nanchen.aiyagirl.module.web;
+
+/**
+ * Author: nanchen
+ * Email: liushilin520@foxmail.com
+ * Date: 2017-04-14  14:41
+ */
+
+public class WebPresenter {
+}
