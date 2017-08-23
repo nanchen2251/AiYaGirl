3c7a7893031f8a096a295b967f193676a952300a
刘世麟 <503233512@qq.com>
2017-08-23 15:39:39 +0800

    更新Activity管理器

--- app/src/main/java/com/nanchen/aiyagirl/App.java
+++ app/src/main/java/com/nanchen/aiyagirl/App.java
@@ -1,15 +1,19 @@
 package com.nanchen.aiyagirl;
 
+import android.app.Activity;
 import android.app.Application;
 
 import com.nanchen.aiyagirl.utils.Utils;
 import com.squareup.leakcanary.LeakCanary;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import cn.bingoogolapple.swipebacklayout.BGASwipeBackManager;
 
 /**
  * 应用程序
- *
+ * <p>
  * Author: nanchen
  * Email: liushilin520@foxmail.com
  * Date: 2017-04-07  14:41
@@ -17,14 +21,17 @@ import cn.bingoogolapple.swipebacklayout.BGASwipeBackManager;
 
 public class App extends Application {
     private static App INSTANCE;
+    private Set<Activity> mActivities;
 
-    public static App getInstance() {
+    public static synchronized App getInstance() {
         return INSTANCE;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        INSTANCE = this;
 
         // 初始化 LeakCanary
         if (LeakCanary.isInAnalyzerProcess(this)) {
@@ -36,7 +43,32 @@ public class App extends Application {
 
         BGASwipeBackManager.getInstance().init(this);
         ConfigManage.INSTANCE.initConfig(this);
-        INSTANCE = this;
         Utils.init(this);
+    }
+
+    public void addActivity(Activity activity) {
+        if (mActivities == null) {
+            mActivities = new HashSet<>();
+        }
+        mActivities.add(activity);
+    }
+
+    public void removeActivity(Activity activity) {
+        if (mActivities != null) {
+            mActivities.remove(activity);
+        }
+    }
+
+    public void exitApp() {
+        if (mActivities != null) {
+            synchronized (mActivities) {
+                for (Activity activity :
+                        mActivities) {
+                    activity.finish();
+                }
+            }
+        }
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(0);
     }
 }
